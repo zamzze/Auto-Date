786cb49264eef3415587540deba0c155627dc813
Samuel Cerron Huertas <61814587+zamzze@users.noreply.github.com>
2026-01-07 23:55:50 -0500

    Update driver.py

--- src/appium/driver.py
+++ src/appium/driver.py
@@ -4,20 +4,21 @@ from appium.options.android import UiAutomator2Options
 options = UiAutomator2Options()
 
 options.platform_name = "Android"
-options.device_name = "Android Emulator"
+options.device_name = "RG89VKVOAEIVZLOF"
 options.automation_name = "UiAutomator2"
-options.app_package = "com.Meyo.app"
-options.app_activity = ".MainActivity"
 
-# CLAVES para dispositivo físico
-options.disable_hidden_api_policy = True
-options.ignore_hidden_api_policy_error = True
-
-# Opcional pero recomendable
+# NO lanzar ninguna app
 options.no_reset = True
 options.new_command_timeout = 300
 
+# EVITAR instalación / resolución
+options.skip_server_installation = True
+options.skip_device_initialization = True
+
+options.disable_hidden_api_policy = True
+options.ignore_hidden_api_policy_error = True
+
 driver = webdriver.Remote(
-    command_executor="http://localhost:4723",
+    command_executor="http://127.0.0.1:4723",
     options=options
 )
